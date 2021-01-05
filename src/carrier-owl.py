c60c8bd78f8cec0813df95d63ee08b5c9dd0f53a
kotapiku <ice08235523@gmail.com>
2021-01-06 00:09:15 +0900

    implement multiple subjects

--- src/carrier-owl.py
+++ src/carrier-owl.py
@@ -45,6 +45,72 @@ def get_articles_info(subject):
     id_list = bs.find_all(class_='list-identifier')
     return id_list
 
+# for multiple subjects
+def get_articles_infos(subjects):
+    weekday_dict = {0: 'Mon', 1: 'Tue', 2: 'Wed', 3: 'Thu',
+                  4: 'Fri', 5: 'Sat', 6: 'Sun'}
+    id_list = []
+    for subject in subjects:
+        url = f'https://arxiv.org/list/{subject}/pastweek?show=100000'
+        response = requests.get(url)
+        html = response.text
+        year = datetime.date.today().year
+
+        # いつの論文データを取得するか
+        bs = BeautifulSoup(html)
+        h3 = bs.find_all('h3')
+        wd = weekday_dict[datetime.datetime.today().weekday()]
+        day = datetime.datetime.today().day
+        today = f'{wd}, {day}'
+
+        # 今日、新しい論文が出てるかどうか(土日とか休みみたい)
+        if today in h3[0].text:
+            idx = 2
+        else:
+            idx = 1
+        articles_html = html.split(f'{year}</h3>')[idx]   # <--------- 要注意
+
+        # 論文それぞれのurlを取得
+        bs = BeautifulSoup(articles_html)
+        id_list.extend(bs.find_all(class_='list-identifier'))
+    return id_list
+
+def generate_scripts(id_list):
+    urls = []
+    titles = []
+    abstracts = []
+    words = []
+    for id_ in progress_bar(id_list):
+        a = id_.find('a')
+        _url = a.get('href')
+        url = 'https://arxiv.org'+_url
+
+        response = requests.get(url)
+        html = response.text
+
+        bs = BeautifulSoup(html)
+        try:
+            title = bs.find('meta', attrs={'property': 'og:title'})['content']
+            abstract = bs.find(
+                    'meta',
+                    attrs={'property': 'og:description'})['content']
+        except:
+            continue
+
+        title_trans = get_translated_text('ja', 'en', title)
+        abstract = abstract.replace('\n', '')
+        abstract_trans = get_translated_text('ja', 'en', abstract)
+        abstract_trans = textwrap.wrap(abstract_trans, 40)  # 40行で改行
+        abstract_trans = '\n'.join(abstract_trans)
+
+        urls.append(url)
+        titles.append(title_trans)
+        abstracts.append(abstract_trans)
+        words.append(hit_kwd_list)
+
+    results = [urls, titles, abstracts, words]
+
+    return results
 
 def serch_keywords(id_list, keywords_dict):
     urls = []
@@ -182,8 +248,8 @@ def get_config():
 def main():
     config = get_config()
     slack = slackweb.Slack(url=os.getenv("SLACK_ID"))
-    id_list = get_articles_info(config['subject'])
-    results = serch_keywords(id_list, config['keywords'])
+    id_list = get_articles_infos(config['subjects'])
+    results = generate_scripts(id_list)
     send2slack(results, slack)
 
 
